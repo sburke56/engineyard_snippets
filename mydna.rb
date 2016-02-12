241b9722b605129e29ca8bf20686ddff617b5abd
Stephen Burke <steve.burke.56@gmail.com>
2016-02-11 19:00:43 -0500

    * Comments.  We are spinning through instances in dna.json and   matching up the instance_id from the instance_api.yml file to see   what instance we are currently on.

--- mydna.rb
+++ mydna.rb
@@ -3,9 +3,11 @@
 require 'json'
 require 'yaml'
 
+# in production file is located at /etc/chef/dna.json
 dnaFile = File.read('./dna.json')
 dnaJson = JSON.parse(dnaFile)
 
+# in production file is located at /etc/engineyard/instance_api.yml
 instance_apiYaml = YAML.load_file('./instance_api.yml')
 
 instances = dnaJson["engineyard"]["environment"]["instances"]
@@ -14,6 +16,8 @@ printf("%-20s %-15s\n", "AWS Instance ID:",  dnaJson["engineyard"]["this"])
 printf("%-20s %-15s\n", "Role:", dnaJson["engineyard"]["environment"]["name"])
 printf("\n")
 
+# Spin through the instances and see which one we are currently using by
+# matching the instance_id from the instance_api.yml file
 instances.each do |i|
   if i["instance_api_config"]["instance_id"] == instance_apiYaml["instance_id"]
     printf("%-20s %-15s\n", "Public Hostname:", i["public_hostname"])
